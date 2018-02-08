f8b137d588181053d1680ff4c13406eb5555ec22
Jesse Baze <35582566+JcBaze@users.noreply.github.com>
2018-02-08 13:22:04 -0600

    Update main.cpp

--- Assignments/program_1/main.cpp
+++ Assignments/program_1/main.cpp
@@ -1,9 +1,8 @@
 /*
 * @ProgramName: Program 1
 * @Author: Jesse Baze
-* @Description: This program reads the txt. (RGB) values of an image
-* allowing the user to pick any of the 5 options to manipulate the
-* image to their choice.
+  @Description: This program reads a txt. file of (RGB) values of an image
+   allowing the user to pick any of the 5 options to manipulate the image.
 * @Course: 1063 Data Structures
 * @Semester: Spring 2018
 * @Date: 1-28-2018
@@ -19,6 +18,8 @@ using namespace std;
 int main()
 {
 	ChangeImage edImage;
+	edImage.Readfile();
 	edImage.userinput();
+	edImage.Printfile();
 	return 0;
 }
